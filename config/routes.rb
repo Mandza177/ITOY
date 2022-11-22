b7ac9db983c7acf1ad382241d4c05a58ebc34a36
Sam Mano <sammano44@gmail.com>
2022-11-22 12:11:27 +0100

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "pages#home"
+  namespace :owner do
+    resources :toys, only: [:index, :new, :create]
+    resources :rents, except: [:index, :new, :show, :create, :edit, :update, :destroy] do
+      member do
+        patch :accept
+        patch :reject
+      end
+    end
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :toys, only: [:index, :show] do
+    resources :rents, only: :create
+  end
+
+  resource :profile, only: [:show]
+
 end

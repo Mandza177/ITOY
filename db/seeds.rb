3e542805a80bb80d0d9b73b2d89890181916203e
arnaud thiebart <arno.thieb@gmail.com>
2022-11-22 17:59:42 +0100

    schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Toy.create(title: "Faucon millennium", description: "You can build the utlimate Lego starWars", price: 150, address: "Nantes")
+Toy.create(title: "Eiffel Tower", description: "Build the biggest Lego ever made", price: 100, address: "Paris")
+Toy.create(title: "Bugatti Chiron", description: "Discover the ultimate french supercar", price: 130, address: "Bordeaux")
+Toy.create(title: "Ferrari Daytona SP3", description: "Build the brand new supercar from Ferrari", price: 120, address: "Rennes")
+Toy.create(title: "The Taj Mahal", description: "The Taj Mahal is a incredible monument", price: 100, address: "Reims")
+Toy.create(title: "The Titanic", description: "the longest Lego build", price: 150, address: "Brest")
+Toy.create(title: "Bonsaï", description: "There's a small tree", price: 30, address: "La Rochelle")
+Toy.create(title: "the Poudlard Express", description: "Get your ticket to Poudlard", price: 120, address: "Toulouse")
+Toy.create(title: "The starry night", description: "Create your Van Gogh", price: 90, address: "Nice")
+Toy.create(title: "Apollo Saturn V", description: "Your next fly to the moon", price: 50, address: "Orléans")
+Toy.create(title: "Razor Crest", description: "Who want to become the next Mandalorian?", price: 140, address: "Lyon")
+Toy.create(title: "AT-AT", description: "Killer Dog", price: 180, address: "Bordeaux")
+Toy.create(title: "R2-D2", description: "Your new Best-friend", price: 90, address: "Poitiers")
+Toy.create(title: "Imperial Star Destroyer", description: "Your uber's just arrived", price: 200, address: "Alençon")

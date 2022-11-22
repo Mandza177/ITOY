4466c06666a00fdd99c25a1fc1ffe60d065026db
Yann Lecocq <mandza@hotmail.fr>
2022-11-22 17:27:45 +0100

    models to et user has one attached

--- app/models/toy.rb
+++ app/models/toy.rb
@@ -4,4 +4,6 @@ class Toy < ApplicationRecord
   validates :address, presence: true
   validates :price, numericality: { only_integer: true }, presence: true
   validates :description, length: { minimum: 15 }, presence: true
+
+  has_one_attached :photo
 end

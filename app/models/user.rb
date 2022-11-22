64c640c68d66ed0164372e9be1ea8a03d4a23f71
Yann Lecocq <mandza@hotmail.fr>
2022-11-22 14:53:55 +0100

    création migration colonne name dans user et validates name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # has_many :toys, dependent: destroy
+  # has_many :rents, dependent: destroy
+
+  validates :name, presence: true, uniqueness: true
 end

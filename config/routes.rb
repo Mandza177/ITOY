3c83baee69e008ca3ea9b214a0db91f6dc8e622b
Sam Mano <sammano44@gmail.com>
2022-11-23 12:34:37 +0100

    homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
-root "pages#home"
-namespace :owner do
-  resources :toys, only: [:index, :new, :create]
-  resources :rents, except: [:index, :new, :show, :create, :edit, :update, :destroy] do
-    member do
-      patch :accept
-      patch :reject
+  root "pages#home"
+    namespace :owner do
+      resources :toys, only: [:index, :new, :create]
+      resources :rents, except: [:index, :new, :show, :create, :edit, :update, :destroy] do
+        member do
+          patch :accept
+          patch :reject
+        end
+      end
     end
-  end
-end
 
-resources :toys, only: [:index, :show] do
-  resources :rents, only: :create
-end
+    resources :toys, only: [:index, :show] do
+      resources :rents, only: :create
+    end
 
-resource :profile, only: [:show]
-  end
+    resource :profile, only: [:show]
+end

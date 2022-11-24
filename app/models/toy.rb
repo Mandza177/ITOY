4bf549e6b3e10677e022cdfc70ff36bd7332681d
Pascal Billerach <p.billerach@gmail.com>
2022-11-24 11:36:26 +0100

    Migration : remplacer address par city dans Toys

--- app/models/toy.rb
+++ app/models/toy.rb
@@ -7,4 +7,11 @@ class Toy < ApplicationRecord
   validates :description, length: { minimum: 15 }, presence: true
 
   has_one_attached :photo
+
+  include PgSearch::Model
+    pg_search_scope :search_by_title_and_description,
+    against: [ :title, :description ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
